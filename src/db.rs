a51c878e91644d92b5de661d03f62329bf24ae4b
Nitin Gupta <nitingupta910@gmail.com>
2017-06-13 10:59:36 -0700

    Allow creating iterator_cf with custom read options

--- src/db.rs
+++ src/db.rs
@@ -824,6 +824,14 @@ impl DB {
         DBIterator::new_cf(self, cf_handle, &opts, mode)
     }
 
+    pub fn iterator_cf_opt(&self,
+                           cf_handle: ColumnFamily,
+                           mode: IteratorMode,
+                           readopts: &ReadOptions)
+                           -> Result<DBIterator, Error> {
+        DBIterator::new_cf(self, cf_handle, &readopts, mode)
+    }
+
     pub fn raw_iterator(&self) -> DBRawIterator {
         let opts = ReadOptions::default();
         DBRawIterator::new(self, &opts)
@@ -1153,10 +1161,15 @@ impl ReadOptions {
     // TODO add snapshot setting here
     // TODO add snapshot wrapper structs with proper destructors;
     // that struct needs an "iterator" impl too.
-    #[allow(dead_code)]
-    fn fill_cache(&mut self, v: bool) {
+    pub fn fill_cache(&mut self, v: bool) {
         unsafe {
             ffi::rocksdb_readoptions_set_fill_cache(self.inner, v as c_uchar);
+        }
+    }
+
+    pub fn verify_checksum(&mut self, v: bool) {
+        unsafe {
+            ffi::rocksdb_readoptions_set_verify_checksums(self.inner, v as c_uchar);
         }
     }
 
